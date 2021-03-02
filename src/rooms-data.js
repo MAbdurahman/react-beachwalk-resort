27a459e8d2de6d972f9478fc130007b0c91f38df
MAbdurahman <mdbdrrhm@gmail.com>
2021-03-02 06:38:41 -0600

    Add rooms-data.js file

---
+++ src/rooms-data.js
@@ -0,0 +1,31 @@
+// import room1 from '../../images/room1.jpeg';
+import room1 from '../src/img/room1.jpeg';
+import room2 from '../src/img/room2.jpeg';
+import room3 from '../src/img/room3.jpeg';
+
+export default [
+	{
+		id: 1,
+		img: room1,
+		title: 'basic economy',
+		info:
+			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est quisquam corrupti dicta tempora sequi sapiente!',
+		price: 125,
+	},
+	{
+		id: 2,
+		img: room2,
+		title: 'presidential suite',
+		info:
+			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est quisquam corrupti dicta tempora sequi sapiente!',
+		price: 250,
+	},
+	{
+		id: 3,
+		img: room3,
+		title: 'standard queen room',
+		info:
+			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est quisquam corrupti dicta tempora sequi sapiente!',
+		price: 375,
+	},
+];
