54c1090c4c337f57623dd5bdedebef7ce4fe9056
MAbdurahman <mdbdrrhm@gmail.com>
2021-03-02 20:13:15 -0600

    Add Banner Component with helper functions and media query

---
+++ src/components/global/Banner.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import styled, { css, keyframes } from 'styled-components';
+import {
+	setColor,
+	setRem,
+	setLetterSpacing,
+	setBorder,
+	media,
+} from './../../styles/Styles';
+
+const Banner = ({ children, className, greeting, title, text }) => {
+	return (
+		<div className={className}>
+			<h1>
+				{greeting} <span>{title}</span>{' '}
+			</h1>
+			<div className='info'>
+				<p>{text}</p>
+				{children}
+			</div>
+		</div>
+	);
+};
+
+const BannerWrapper = styled(Banner)`
+	background: rgba(0, 0, 0, 0.7);
+	text-align: center;
+	padding: ${setRem(60)} ${setRem(32)};
+	${setLetterSpacing(3)};
+	color: ${setColor.mainWhite};
+	h1 {
+		text-transform: capitalize;
+		font-size: ${setRem(48)};
+		color: ${setColor.primaryColor};
+		span {
+			color: ${setColor.mainWhite};
+		}
+	}
+	p {
+		width: 85%;
+		margin: 0 auto;
+	}
+	${media.tablet` width: 70vw;
+      ${setBorder({ width: '6px', color: setColor.primaryColor })};
+      p {
+         width: 60%;
+      }`}
+	h1 {
+		/* animation */
+	}
+	.info {
+		/* animation */
+	}
+`;
+
+export default BannerWrapper;
--- src/components/page/home/header/Header.jsx
+++ src/components/page/home/header/Header.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Hero from '../../../global/Hero';
 import homeImage from '../../../../img/homeBcg.jpeg';
+import Banner from './../../../global/Banner';
 
 export default function Header() {
-   return (
-      <Hero img={homeImage} >
-         <h1>Beachwalk Resort</h1>
-      </Hero>
-   );
-};
+	return (
+		<Hero img={homeImage}>
+			<Banner
+				greeting='welcome to'
+				title='beachwalk resort'
+				text='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi provident eos sapiente deleniti in quisquam.'
+			/>
+		</Hero>
+	);
+}
