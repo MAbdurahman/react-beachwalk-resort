9817a7660fa3025526505c6841bf5a7df04ba63d
MAbdurahman <mdbdrrhm@gmail.com>
2021-03-02 21:18:03 -0600

    Complete hero_component-section with Header, Bannner, and Button

--- src/components/page/home/header/Header.jsx
+++ src/components/page/home/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Hero from '../../../global/Hero';
 import homeImage from '../../../../img/homeBcg.jpeg';
 import Banner from './../../../global/Banner';
+import {PrimaryBtn} from '../../../global/Buttons'
 
 export default function Header() {
 	return (
@@ -10,7 +11,9 @@ export default function Header() {
 				greeting='welcome to'
 				title='beachwalk resort'
 				text='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi provident eos sapiente deleniti in quisquam.'
-			/>
+			>
+				<PrimaryBtn t='1rem'>view details</PrimaryBtn>
+			</Banner>
 		</Hero>
 	);
 }
