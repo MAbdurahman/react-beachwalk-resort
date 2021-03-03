9fb2997a2e0eec86192de6aba009837fe341bac2
MAbdurahman <mdbdrrhm@gmail.com>
2021-03-03 05:37:09 -0600

    Complete Section and Title components

---
+++ src/components/page/about/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Section from './../../global/Section';
+import Title from './../../global/Title';
+
+export default function About() {
+   return (
+      <Section>
+         <Title title='about us' />
+      </Section>
+   );
+};
--- src/pages/home-page/HomePage.jsx
+++ src/pages/home-page/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './../../components/page/home/header/Header';
+import About from './../../components/page/about/About';
 
 export default function HomePage() {
 	return (
 		<>
 			<Header />
+			<About />
 		</>
 	);
 }
